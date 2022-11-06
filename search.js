e3b5c2b715c4bd291d19acdd68a8c0da915b563e
st4lwolf <92603563+Malwprotector@users.noreply.github.com>
2022-11-06 17:27:19 +0100

    Add files via upload

---
+++ search.js
@@ -0,0 +1,15 @@
+// JavaScript code
+function search() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('items');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
